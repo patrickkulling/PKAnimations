89f1e53910d3b606b93b64276a487ace9c8b64e2
Patrick Kulling <patrick.kulling@wooga.com>
2013-06-02 15:15:41 +0200

    add back easing equations

---
+++ PKAnimations/ease/PKEaseBack.m
@@ -0,0 +1,65 @@
+/*
+============================================================================================
+ Easing Equations
+ (c) 2003 Robert Penner, all rights reserved.
+ This work is subject to the terms in http://www.robertpenner.com/easing_terms_of_use.html.
+============================================================================================
+
+TERMS OF USE - EASING EQUATIONS
+
+Open source under the BSD License.
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+    * Neither the name of the author nor the names of contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#import "PKEaseBack.h"
+
+static const float overShoot = 1.70158;
+
+@implementation PKEaseBack {
+}
+@end
+
+@implementation PKEaseBackIn {
+}
+
+- (CGFloat)getValue: (CGFloat)currentTime startValue: (CGFloat)startValue changeByValue: (CGFloat)changeByValue duration: (CGFloat)duration {
+    return changeByValue * (currentTime /= duration) * currentTime * ((overShoot + 1) * currentTime - overShoot) + startValue;
+}
+
+@end
+
+@implementation PKEaseBackOut {
+}
+
+- (CGFloat)getValue: (CGFloat)currentTime startValue: (CGFloat)startValue changeByValue: (CGFloat)changeByValue duration: (CGFloat)duration {
+    return changeByValue * ((currentTime = currentTime / duration - 1) * currentTime * ((overShoot + 1) * currentTime + overShoot) + 1) + startValue;
+}
+
+@end
+
+@implementation PKEaseBackInOut {
+}
+
+- (CGFloat)getValue: (CGFloat)currentTime startValue: (CGFloat)startValue changeByValue: (CGFloat)changeByValue duration: (CGFloat)duration {
+    float mutableOverShoot = overShoot;
+
+    if ((currentTime /= duration / 2) < 1)
+        return changeByValue / 2 * (currentTime * currentTime * (((mutableOverShoot *= (1.525)) + 1) * currentTime - mutableOverShoot)) + startValue;
+
+    return changeByValue / 2 * ((currentTime -= 2) * currentTime * (((mutableOverShoot *= (1.525)) + 1) * currentTime + mutableOverShoot) + 2) + startValue;
+}
+
+@end
